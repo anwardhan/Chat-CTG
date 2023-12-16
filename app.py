055615d96dcd04ed63e7cf053ddf931244c767e2
anwardhan <anwar.dhan@gmail.com>
2023-12-15 19:22:52 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,124 @@
+import streamlit as st
+import openai
+from langchain.chains import ConversationalRetrievalChain
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.prompts.chat import SystemMessagePromptTemplate
+
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+
+@st.cache_resource
+def load_chain():
+    """
+    The `load_chain()` function initializes and configures a conversational retrieval chain for
+    answering user questions.
+    :return: The `load_chain()` function returns a ConversationalRetrievalChain object.
+    """
+
+    # Load OpenAI embedding model
+    embeddings = OpenAIEmbeddings()
+
+    # Load OpenAI chat model
+    llm = ChatOpenAI(temperature=0)
+
+    # Load our local FAISS index as a retriever
+    vector_store = FAISS.load_local("faiss_index", embeddings)
+    retriever = vector_store.as_retriever(search_kwargs={"k": 3})
+
+    # Create memory 'chat_history'
+    memory = ConversationBufferWindowMemory(k=3, memory_key="chat_history")
+
+    # Create the Conversational Chain
+    chain = ConversationalRetrievalChain.from_llm(llm,
+                                                  retriever=retriever,
+                                                  memory=memory,
+                                                  get_chat_history=lambda h: h,
+                                                  verbose=True)
+
+    # Create system prompt
+    template = """
+    You are an AI assistant for answering questions about the Close the Gap Summer Fellowship.
+    You are given the following extracted parts of a long document and a question. Provide a conversational answer.
+    If you don't know the answer, just say 'Sorry, I don't know... 😔'.
+    Don't try to make up an answer.
+    If the question is not about the Close the Gap Fellowship politely ask them to reach out to their Cohort Leader for a response.
+
+    {context}
+    Question: {question}
+    Helpful Answer:"""
+
+    # Add system prompt to chain
+    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], template=template)
+    chain.combine_docs_chain.llm_chain.prompt.messages[0] = SystemMessagePromptTemplate(prompt=QA_CHAIN_PROMPT)
+
+    return chain
+
+
+import time
+import streamlit as st
+#from utils import load_chain
+
+# Custom image for the app icon and the assistant's avatar
+company_logo = "https://i.ibb.co/bdhDWyn/ctg-lettermark-red-transparent.png"
+
+
+
+
+
+# Configure streamlit page
+st.set_page_config(
+    page_title="Your Notion Chatbot",
+    page_icon=company_logo
+)
+
+# Initialize LLM chain in session_state
+if 'chain' not in st.session_state:
+    st.session_state['chain']= load_chain()
+
+# Initialize chat history
+if 'messages' not in st.session_state:
+    # Start with first message from assistant
+    st.session_state['messages'] = [{"role": "assistant",
+                                  "content": "Hi! I am Close The Gap's smart AI. How can I help you today?"}]
+
+# Display chat messages from history on app rerun
+# Custom avatar for the assistant, default avatar for user
+for message in st.session_state.messages:
+    if message["role"] == 'assistant':
+        with st.chat_message(message["role"], avatar=company_logo):
+            st.markdown(message["content"])
+    else:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+# Chat logic
+if query := st.chat_input("Ask me anything"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": query})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(query)
+
+    with st.chat_message("assistant", avatar=company_logo):
+        message_placeholder = st.empty()
+        # Send user's question to our chain
+        result = st.session_state['chain']({"question": query})
+        response = result['answer']
+        full_response = ""
+
+        # Simulate stream of response with milliseconds delay
+        for chunk in response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+
+    # Add assistant message to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+
